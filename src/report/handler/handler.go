dd66ed5cd0ed48ddff7e9ad0efe82f90b5f4e57c
Yuda Cogati <yudapc@gmail.com>
2019-07-12 01:14:33 +0700

    fix sales report

--- src/report/handler/handler.go
+++ src/report/handler/handler.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"net/http"
 	"sorabel/helpers"
+	ItemModel "sorabel/src/item/model"
 	"sorabel/src/report/model"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
@@ -12,13 +14,57 @@ import (
 func ItemValueReport(db *gorm.DB) echo.HandlerFunc {
 	return func(context echo.Context) error {
 		data := model.GenerateItemValueReport(db)
-		return helpers.ToJson(context, http.StatusOK, "succesfully", data)
+		const layout = "2 Jan 2006"
+		t := time.Now()
+		DateTime := t.Format(layout)
+		var totalItems int
+		var totalValues float64
+		items, _ := ItemModel.GetItems(db, context)
+		totalSku := len(items)
+		for _, value := range data {
+			totalItems = totalItems + value.TotalItemReceived
+			totalValues = totalValues + value.PurchasePrice
+		}
+		header := helpers.HashObject{
+			"DateTime":   DateTime,
+			"totalSku":   totalSku,
+			"totalItems": totalItems,
+		}
+		response := helpers.HashObject{
+			"header": header,
+			"items":  data,
+		}
+		return helpers.ToJson(context, http.StatusOK, "succesfully", response)
 	}
 }
 
 func SalesReport(db *gorm.DB) echo.HandlerFunc {
 	return func(context echo.Context) error {
 		data := model.GenerateSalesReport(db)
-		return helpers.ToJson(context, http.StatusOK, "succesfully", data)
+		const layout = "2 Jan 2006"
+		t := time.Now()
+		DateTime := t.Format(layout)
+		var totalProfit float64
+		var totalItems int
+		var totalOmzet float64
+		totalTransactions := len(data)
+		for _, value := range data {
+			totalOmzet = totalOmzet + value.Total
+			totalProfit = totalProfit + value.Profit
+			totalItems = totalItems + value.TotalQty
+		}
+		header := helpers.HashObject{
+			"Date":              DateTime,
+			"totalOmzet":        totalOmzet,
+			"totalProfit":       totalProfit,
+			"totalItems":        totalItems,
+			"totalTransactions": totalTransactions,
+		}
+
+		response := helpers.HashObject{
+			"header": header,
+			"items":  data,
+		}
+		return helpers.ToJson(context, http.StatusOK, "succesfully", response)
 	}
 }
--- src/report/model/model.go
+++ src/report/model/model.go
@@ -1,23 +1,30 @@
 package model
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 )
 
 type ItemValueReport struct {
-	Sku               string `json:"sku"`
-	Name              string `json:"name"`
-	TotalQtyPurchased int    `json:"total_qty_purchased"`
-	TotalItemReceived int    `json:"total_item_received"`
-	TotalStock        int    `json:"total_stock"`
+	Sku               string  `json:"sku"`
+	Name              string  `json:"name"`
+	TotalQtyPurchased int     `json:"total_qty_purchased"`
+	TotalItemReceived int     `json:"total_item_received"`
+	TotalStock        int     `json:"total_stock"`
+	PurchasePrice     float64 `json:"purhase_price"`
 }
 
 type SalesReport struct {
-	Sku              string  `json:"sku"`
-	Name             string  `json:"name"`
-	TotalQty         int     `json:"total_qty"`
-	TotalProfit      float64 `json:"total_profit"`
-	TotalTransaction float64 `json:"total_transaction"`
+	InvoiceNumber string    `json:"invoice_number"`
+	CreatedAt     time.Time `json:"created_at"`
+	Sku           string    `json:"sku"`
+	Name          string    `json:"name"`
+	TotalQty      int       `json:"total_qty"`
+	SellingPrice  float64   `json:"selling_price"`
+	Total         float64   `json:"total"`
+	PurchasePrice float64   `json:"purchase_price"`
+	Profit        float64   `json:"profit"`
 }
 
 func GenerateItemValueReport(db *gorm.DB) []ItemValueReport {
@@ -28,7 +35,8 @@ func GenerateItemValueReport(db *gorm.DB) []ItemValueReport {
 		i.name AS name, 
 		(SELECT SUM(qty) FROM purchase_details WHERE sku = i.sku) AS total_qty_purchased,
 		(SELECT SUM(item_received) FROM purchase_details WHERE sku = i.sku) AS total_item_received,
-		i.stock AS total_stock
+		i.stock AS total_stock,
+		i.purchase_price AS purchase_price
 	FROM
 		items i
 	`
@@ -40,14 +48,20 @@ func GenerateItemValueReport(db *gorm.DB) []ItemValueReport {
 func GenerateSalesReport(db *gorm.DB) []SalesReport {
 	var salesReports []SalesReport
 	sql := `
-	SELECT 
-		i.sku AS sku,
-		i.name AS name,
-		(SELECT SUM(qty) FROM sales_details WHERE sku = i.sku) AS total_qty,
-		(SELECT SUM(profit) FROM sales_details WHERE sku = i.sku) AS total_profit,
-		(SELECT COUNT(*) FROM sales_details WHERE sku = i.sku) AS total_transaction
-	FROM
-		items i
+	SELECT
+		s.invoice_number AS invoice_number,
+		s.created_at AS created_at,
+		sd.sku AS sku, 
+		sd.name AS name,
+		SUM(sd.qty) AS total_qty,
+		i.selling_price AS selling_price,	
+		SUM(sd.qty) * i.selling_price AS total,
+		i.purchase_price AS purchase_price,	
+		SUM(sd.profit) AS profit
+	FROM sales_details sd
+	LEFT JOIN items AS i ON i.sku = sd.sku
+	LEFT JOIN sales AS s ON s.id = sd.sales_id
+	GROUP BY sd.sku
 	`
 	db.Raw(sql).Scan(&salesReports)
 	return salesReports
